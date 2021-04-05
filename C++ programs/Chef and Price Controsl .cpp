ee3af072843b3f4e21987712898678b18394e328
yprateek136 <yprateek195@gmail.com>
2021-04-06 01:00:05 +0530

    Chef and Price Controls

---
+++ C++ programs/Chef and Price Controsl .cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k,sum=0,result=0,bsum=0;
+		cin>>n>>k;
+		int a[1000000];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			sum+=a[i];
+			if(a[i]>k)
+			{
+				bsum+=k;	
+			}
+			else
+			{
+				bsum+=a[i];
+			}
+		}
+		result=sum-bsum;
+		cout<<result;
+	}
+	return 0;
+}
