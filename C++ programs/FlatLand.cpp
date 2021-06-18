de1a3696f350f6d4406f64592d082e8d3c5cb2bc
yprateek136 <yprateek195@gmail.com>
2021-06-18 12:18:01 +0530

    Flatland

---
+++ C++ programs/FlatLand.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,count=0;
+		cin>>n;
+		while(n>0)
+		{
+			int x = sqrt(n);
+			cout<<x<<endl;
+			n=n-(x*x);
+			count++;
+		}
+		cout<<count<<endl;
+	}
+}
