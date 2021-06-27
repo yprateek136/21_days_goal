b2e6800e2dee04f29c4d4ef36ffff6b26c2cadd0
yprateek136 <yprateek195@gmail.com>
2021-06-27 10:50:06 +0530

    modify code

---
+++ C++ programs/ATM(CODE CHEF HS08TEST).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int withdraw;
+	float balance;
+	cin>>withdraw;
+	cin>>balance;
+	
+	if((withdraw%5==0)&&(balance>=(withdraw+0.50)))
+	{
+	    balance=balance-(withdraw+0.50);
+	    cout << fixed << setprecision(2) << balance<<endl;
+	}
+	else
+    	cout<<balance;
+	return 0;
+}
