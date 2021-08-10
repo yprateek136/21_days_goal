8f9497b91ddb935364d336dd33445fd084126d2c
yprateek136 <yprateek195@gmail.com>
2021-08-10 15:48:26 +0530

    array

---
+++ C++ programs/Fighting the darkness.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{   
+public:
+    int maxDays(int arr[],int n)
+    {
+      
+        for(int i=0;i<n;i++)
+        {
+            res=max(res,arr[i]);
+        }
+        return res;
+    }
+};
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout << ob.maxDays(arr, n) << "\n";
+    }
+    return 0;
+} 
