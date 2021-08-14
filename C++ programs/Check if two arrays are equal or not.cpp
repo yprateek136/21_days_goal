270327914f3bc87be9bf81301ed4be1f4374176d
yprateek136 <yprateek195@gmail.com>
2021-08-15 00:40:23 +0530

    modify

--- C++ programs/Check if two arrays are equal or not.cpp
+++ C++ programs/Check if two arrays are equal or not.cpp
@@ -34,12 +34,6 @@ int main()
         // increase the count of elements in first array
         for(ll i=0;i<n;i++)
             cin >> arr[i];
-        
-        
-        // iterate through another array
-        // and decrement the count of elements
-        // in the map in which frequency of elements
-        // is stored for first array
         for(ll i=0;i<n;i++)
             cin >> brr[i];
         Solution ob;
