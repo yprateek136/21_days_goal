3061ae1ee9e1ce480d39ec393203a6ecbcf9f005
yprateek136 <yprateek195@gmail.com>
2021-08-14 11:37:47 +0530

    array

---
+++ C++ programs/Last index of One.cpp
@@ -0,0 +1,34 @@
+// Last index of One 
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int lastIndex(string s) 
+    {
+        int n=s.length();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='1')
+            {
+                return i;
+                break;
+            }
+        }
+        return -1;
+    }
+
+};
+
+//Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.lastIndex(s) << endl;
+    }
+    return 0;
+}
